7ef58c4d7c06076c6a690d64d7c4648d6e903add
jakubpodsiadlo-stack <jakub.podsiadlo@easyeko.pl>
2025-12-09 23:59:26 +0100

    login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,10 @@
 export default function handler(req, res) {
   const { username, password } = req.body;
-  if(username === "admin" && password === "1234"){
+
+  if(username === "DAMIAN" && password === "ZIEMNIAKIIBULKA"){
     res.status(200).json({ token: "demo-token" });
   } else {
     res.status(401).json({ error: "Nieprawidłowe dane" });
   }
 }
+
